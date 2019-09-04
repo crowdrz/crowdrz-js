96b220d864f004f490da652bfa601e0d5f734d84
Antonin Bouscarel <antonin.bouscarel.pro@gmail.com>
2019-09-04 12:19:22 +0200

    change typescript to javascript

--- dist/Crowdrz.d.ts
+++
@@ -1,9 +0,0 @@
-declare const availableScope: string[];
-declare const processList: any;
-declare class Crowdrz {
-    scope: string;
-    key: string;
-    verbose: boolean;
-    constructor(scope: string, key: string);
-    applyProcess(name: string, ressource: any, params: any): void;
-}
--- dist/process/facebook/getComments.d.ts
+++
@@ -1,9 +0,0 @@
-declare const request: any;
-declare const Parser: any;
-declare const fs: any;
-declare let comments: any;
-declare const fields: string[];
-declare const opts: {
-    fields: string[];
-};
-declare const requestComment: (url: string, cb: any) => void;
