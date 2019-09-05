7843cf144395b73460667def0094c05a8e017472
Antonin Bouscarel <antonin.bouscarel.pro@gmail.com>
2019-09-05 18:17:41 +0200

    refacto core

--- demo/getCommentsDemo.js
+++ demo/getCommentsDemo.js
@@ -1,5 +1,9 @@
-const Crowdrz = require('../lib/crowdrz')
+import Crowdrz from '../lib/crowdrz';
 
-let crowdrz = new Crowdrz('facebook', 'EAAOdARHxc0YBAB1tBLkE55o01tf3oBRQnukOW84ahzmZAPPH8nhs6rEqXU6IUO0qzDQ9OnKDnJCYkS4Mm0QdNQXlqOm5U9UiVX1hK5yhK4GFZAQ1JE1XT0zA2JVOzMpJxJUxeJEKuy8SU0gNM5wWTyiYETxwONQ4luyw0SCpM9OLsSXZApgZA1MF5FHwaHUZD');
-let comments = crowdrz.applyProcess('getComments', '2394127887331679');
-console.log('comments', comments);
+const getCommentsDemo = function() {
+  let crowdrz = new Crowdrz();
+  crowdrz.addScope('facebook', 'EAAGnFkxIcgQBAC1q7lmdM36JMx5qQg3ZAYPj5Hdai4pLkgm6MjZARrVRkM1nJRgsLexv3KoKIHoWqXmeqM2QTwQ9ZAYcWppXLDKeBBbwniFxebJpigl3gH7Nr1GtOSUixFCnE2SJUIkkogL56NmreMxKmpUVsGHI1qDjfnGTVqhRWQg6Vwyn4uDsi0C2rxcXPDeWsJ0nQZDZD')
+  crowdrz.facebook.getComments();
+}
+
+getCommentsDemo();
