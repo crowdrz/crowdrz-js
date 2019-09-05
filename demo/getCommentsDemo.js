b306c35f4c61051f2d0457b5237dbf43a6e1b772
Antonin Bouscarel <antonin.bouscarel.pro@gmail.com>
2019-09-06 00:08:08 +0200

    facebook get & get raw services + get comment method

--- demo/getCommentsDemo.js
+++ demo/getCommentsDemo.js
@@ -1,9 +1,10 @@
 const Crowdrz = require('../lib/crowdrz');
 
-const getCommentsDemo = function() {
+const getCommentsDemo = async function() {
   let crowdrz = new Crowdrz();
-  crowdrz.addScope('facebook', 'EAAGnFkxIcgQBAC1q7lmdM36JMx5qQg3ZAYPj5Hdai4pLkgm6MjZARrVRkM1nJRgsLexv3KoKIHoWqXmeqM2QTwQ9ZAYcWppXLDKeBBbwniFxebJpigl3gH7Nr1GtOSUixFCnE2SJUIkkogL56NmreMxKmpUVsGHI1qDjfnGTVqhRWQg6Vwyn4uDsi0C2rxcXPDeWsJ0nQZDZD')
-  crowdrz.facebook.getComments();
+  crowdrz.addScope('facebook', '<api key>')
+  let res = await crowdrz.facebook.getComments('<post id>', 26, crowdrz.facebook.token);
+  console.log('res', res);
 }
 
 getCommentsDemo();
--- test/lib/crowdrz.test.js
+++ test/lib/crowdrz.test.js
@@ -1,16 +1,19 @@
-const chai = require("chai");
+'use strict';
+
+const chai = require('chai');
 const expect = chai.expect;
 
 const Crowdrz = require('../../lib/crowdrz');
 
-describe("Crowdrz Core", () => {
-  it("should be init", () => {
+describe('Crowdrz Core', () => {
+  it('should be init', () => {
     const node = new Crowdrz();
     expect(node).to.be.exist;
   });
 
-  it("should add scope", () => {
+  it('should add scope', () => {
     const node = new Crowdrz();
+    expect(() => node.facebook).to.throw();
     node.addScope('facebook', '123');
     expect(node.facebook).to.be.exist;
   })
