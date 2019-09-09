70d168b6331193252e373ab250c31226c3e3b6a9
Antonin Bouscarel <antonin.bouscarel.pro@gmail.com>
2019-09-09 17:11:01 +0200

    remove token demo file & deduplicate function

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,6 @@
 const Crowdrz = require('../lib/crowdrz');
 
-const apiKey = 'EAAGnFkxIcgQBAMehzed3niJfZAJINBhT86ryKqqkhQ7qAI4asVTTZCKIJwDFfQ9v4nRdlpF7uullM6zCmAt09hnRtlalzs7ENZAWAdyY3QKN8uFKCPHZBPUc4ghQdiM4i47w83FiZCYUeUrQzcKfCPtJcepzaZCa24FqUjZBveOlyl7J5SIoUBS5RoBn6hFaSd2gYTEw1uo0QZDZD';
+const apiKey = '<api key>';
 
 const getMeDemo = async () => {
   const facebook = new Crowdrz('facebook')
@@ -8,6 +8,7 @@ const getMeDemo = async () => {
   const me = await facebook.call('GET', '/me')
   const me2 = await facebook.setMethod('GET').setEndpoint('/me').call()
   const me3 = await facebook.setEndpoint('/me').get()
+  console.log(me, me2, me3)
 }
 
 getMeDemo()
