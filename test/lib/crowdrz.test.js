30c433b0ad534746fdb823804d452d0381aecfed
Antonin Bouscarel <antonin@MacBook-Pro-de-Antonin.local>
2021-08-26 17:23:34 -0400

    fix test

--- test/lib/crowdrz.test.js
+++ test/lib/crowdrz.test.js
@@ -1,13 +1,16 @@
-'use strict';
+'use strict'
 
-const chai = require('chai');
-const expect = chai.expect;
+const chai = require('chai')
+const expect = chai.expect
 
-const Crowdrz = require('../../lib/crowdrz');
+const {Crowdrz} = require('../../dist/crowdrz')
 
 describe('Crowdrz Core', () => {
   it('should be init', () => {
-    const node = new Crowdrz();
-    expect(node).to.be.exist;
-  });
-});
+    const node = new Crowdrz('facebook', '123', '4.3')
+    expect(node).to.be.exist
+    expect(node.apiToken).to.eql('123')
+    expect(node.apiVersion).to.eql('4.3')
+    expect(node.provider).to.eql('facebook')
+  })
+})
