2619003936b5ddea946a767997d82129893e3d88
Antonin Bouscarel <antonin@MacBook-Pro-de-Antonin.local>
2021-08-30 10:23:38 -0400

    change ct name to fix include bug\

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
 import {getData as facebookGetData, facebookServiceInterface} from './facebook/index'
-import {getData as crowdtangleGetData, crowdtangleServiceInterface} from './crowdtangle/index'
+import {getData as crowdtangleGetData, crowdtangleServiceInterface} from './ct/index'
 
 export interface servicesListInterface {
   facebook: facebookServiceInterface
